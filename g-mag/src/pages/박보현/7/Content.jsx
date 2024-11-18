280bc718bd3f6ab678a3eee0878c598829cabd3f
Nope777 <suminwow1@gmail.com>
2024-11-18 17:09:26 +0900

    Add Posts

---
+++ g-mag/src/pages/박보현/7/Content.jsx
@@ -0,0 +1,90 @@
+export default function Content() {
+  return (
+    <>
+      <p>
+      인터넷이 발달된 요즘 세상에서 유튜브와 인스타그램을 사용하지 않는 사람은 극히 드물 것이다. 특히 쇼츠, 릴스와 같은 숏폼 미디어가 급부상하면서 컨텐츠의 전파 속도가 급부상했고, 자연스레 이들이 유행의 주도자가 되었다. 플랫폼에서 하루에도 수백, 수천 개씩 쏟아져 나오는 컨텐츠들이 인터넷을 타고 빠른 속도로 퍼져나가 ‘유행’이 되는 것이다. 그리고 우리는 유행에 뒤쳐지지 않기 위해 수많은 플랫폼에서 수많은 컨텐츠를 열심히 향유한다. 이렇게 인터넷에서 이미지, 동영상, 해시태그, 유행어 등 다양한 형태로 급속도로 확산되며 사회 문화의 일부로 자리잡은 트렌드를 ‘인터넷 밈’이라고 한다. 우리는 바야흐로 인터넷 밈의 시대에 살고 있는 것이다.
+      </p>
+
+      <p>
+      흔히들 독일의 대중음악은 빈약하다고 하지만, 인터넷을 만나고 독일을 넘어서 국제적인 밈이 된 독일 노래들이 있다. 그리고 당신도 오늘 소개할 노래들 중 하나 이상은 반드시 들어 보았을 것이다. 하지만 그게 독일 노래였을 줄은 몰랐을 수도 있다. 그렇다면 게슈탈트 붕괴에 주의하며 함께 들어 보도록 하자.
+      </p>
+
+      <hr />
+
+      <p><strong><center>
+      1. Fool’s Garden – Lemon Tree
+      </center></strong></p>
+
+      <div style={{ position: 'relative', paddingBottom: '56.25%', height: 0, overflow: 'hidden' }}>
+      <iframe style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }} src="https://www.youtube.com/embed/XAFS43NKFag?si=MBtfKKq-cKbb_37W" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      </div>
+
+      <br></br>
+      <center>
+        <span style={{ fontStyle: "italic", backgroundColor: "lightgray"}}>
+        I wonder how, I wonder why
+        </span>
+      </center>
+
+      <p>
+      비단 우리나라뿐만 아니라 전세계의 수많은 젊은이들은 어린 시절 영어 시간에 이 노래를 배워 본 경험이 있다. 전체적으로 템포가 느리고 가사가 직관적이며 사용된 단어가 그다지 어렵지 않아 어린 학생들의 영어 공부에 아주 제격인 곡이다. 그래서인지 영상에 달린 대다수의 댓글들은 이 노래를 부르며 영어를 공부하던 어린 시절의 추억에 대한 내용이다.
+      </p>
+
+      <p>
+      하지만 이 명곡을 탄생시킨 그룹 ‘Fool’s Garden’은 놀랍게도 독일의 록 밴드 그룹이다. 이말인즉슨 &lt;Lemon Tree&gt;는 이 노래로 영어를 배운 수많은 사람들의 예상과는 다르게 독일 가수들이 부른 독일 노래가 된다는 뜻이다. 독일 대중음악의 특징 중 하나가 독일어가 아닌 100% 영어로만 된 가사가 제법 많다는 것인데, &lt;Lemon Tree&gt; 역시 그런 곡 중 하나다. 그러다 보니 독일 노래가 세계인들의 영어 공부에 이용되는 다소 웃지 못할 상황이 발생해 버렸다.
+      </p>
+
+      <hr />
+
+      <p><strong><center>
+      2. Boney M. – Rasputin
+      </center></strong></p>
+
+      <div style={{ position: 'relative', paddingBottom: '56.25%', height: 0, overflow: 'hidden' }}>
+      <iframe style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }} src="https://www.youtube.com/embed/Nl_Eo2QzqU4?si=TfdqIIkNpHJ1eL7L" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      </div>
+
+      <br />
+
+      <center>
+        <span style={{ fontStyle: "italic", backgroundColor: "lightgray"}}>
+        Ra Ra Rasptin, lover of the Russian queen
+        </span>
+      </center>
+
+      <p>
+      때는 러시아 제국 말기, 그리고리 라스푸틴은 혈우병을 앓고 있던 황태자를 치료해준 것으로 차르 부부의 신임을 얻어 단숨에 최고 권력자로 급부상했다. 하지만 그는 권력욕과 성욕에 눈이 멀어 각종 기행과 무자비한 폭정을 이어가다 미스터리한 죽음을 맞게 되었다. 이런 그의 생애와 그에게 속절없이 휘둘리던 러시아 제국을 풍자한 곡이 바로 Boney M.의 &lt;Rasputin&gt;이다. 여기까지만 들으면 평범한 풍자곡과 같은 맥락이다. 하지만 &lt;Rasputin&gt;은 유명 댄스형 리듬게임 &lt;Just Dance&gt; 시리즈에 엄청난 운동량을 자랑하는 고난이도 곡으로 수록되며 인기몰이를 하게 되었다. 수많은 게이머들이 너도나도 달려가 이를 플레이한 인터넷에 올렸고, 그 영상들이 일파만파 퍼져나가며 자연스레 곡이 유명해졌기 때문이다.
+      </p>
+
+      <p>
+      앞서 들어본 &lt;Lemon Tree&gt;와 마찬가지로 가사는 영어로 되어 있지만, &lt;Rasputin&gt; 역시 독일 밴드 Boney. M이 부른 엄연한 독일 노래이다. 그래서인지 노래를 듣다 보면 ‘wunderbar’라는 독일어 단어가 하나 등장하니 귀기울여 들어 보도록 하자.
+      </p>
+
+      <hr />
+
+      <p><strong><center>
+      3. Dschinghis Khan – Moskau
+      </center></strong></p>
+
+      <div style={{ position: 'relative', paddingBottom: '56.25%', height: 0, overflow: 'hidden' }}>
+      <iframe style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }} src="https://www.youtube.com/embed/lyuFLU2Zqz0?si=uvIglskIy2sEF96q" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      </div>
+
+      <br />
+
+      <center>
+        <span style={{ fontStyle: "italic", backgroundColor: "lightgray"}}>
+        Moskau, Moskau, wirf die Gläser an die Wand
+        </span>
+      </center>
+
+      <p>
+      제목인 'Moskau‘는 '모스크바‘의 독일식 표기이다. 1979년에 발매된 곡으로, 발매 시점으로부터 1년 후인 1980년 모스크바 올림픽을 기념하기 위해 만들어졌다고 한다. 한마디로 유명한 몽골인의 이름을 그룹명으로 내건 독일의 그룹이 러시아의 올림픽 개최를 기념하기 위해 수도인 모스크바를 소재로 부른 상당히 글로벌한 노래라고 할 수 있겠다. 하지만 앞서 살펴본 두 노래와는 다르게 가사가 처음부터 끝까지 독일어로 되어 있는 것이 특징이다.
+      </p>
+
+      <p>
+      여러모로 너무 글로벌하긴 해도 엄연한 독일 그룹이 부른 독일 노래지만 결정적인 소재가 러시아와 모스크바이다 보니 러시아와 러시아인들의 각종 기행과 관련된 밈에 배경음악으로 자주 등장했다. 하지만 단순히 배경음악으로만 쓰이기에는 노래와 안무가 어마어마한 중독성을 자랑한 나머지 그 자체로 자연스럽게 밈이 되어버렸다.
+      </p>
+    </>
+  );
+}
